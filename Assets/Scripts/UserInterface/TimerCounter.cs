a6d03e31acf76aad61359a1c1a723fdb59204056
Ilya Molchanov <lolkers.ilya@gmail.com>
2024-06-01 14:07:26 +0300

    Add timer

--- Assets/Scripts/UserInterface/TimerCounter.cs
+++ Assets/Scripts/UserInterface/TimerCounter.cs
@@ -19,30 +19,34 @@ public class TimerCounter : MonoBehaviour
     {
         _currentGameMode = currentGameMode - 1;
         Debug.Log("Start game coroutine");
-        switch (currentGameMode)
-        {
-            case 1: _modeTextPattern = "Easy mode, time left " + _currentTime.ToString() + " sec";
-                break;
-            case 2: _modeTextPattern = "Normal mode, time left " + _currentTime.ToString() + " sec";
-                break;
-            case 3: _modeTextPattern = "Hard mode, time left " + _currentTime.ToString() + " sec";
-                break;
-        }
+
 
         _currentTime = spawners[_currentGameMode].GameDuration;
         StartCoroutine(spawners[_currentGameMode].SpawnGoosesWithDelay());
 
         while (_currentTime > 0)
         {
+            switch (currentGameMode)
+            {
+                case 1:
+                    _modeTextPattern = "Easy mode, time left " + _currentTime.ToString("F0") + " sec";
+                    break;
+                case 2:
+                    _modeTextPattern = "Normal mode, time left " + _currentTime.ToString("F0") + " sec";
+                    break;
+                case 3:
+                    _modeTextPattern = "Hard mode, time left " + _currentTime.ToString("F0") + " sec";
+                    break;
+            }
+
             _currentTime -= Time.deltaTime;
             UpdateTimerText(timerText, _modeTextPattern);
             yield return null;
         }
     }
-    private void UpdateTimerText(TextMeshProUGUI tmpText, string textTemplate)
+    private void UpdateTimerText(TextMeshProUGUI tmpText, string textPattern)
     {
-        Debug.Log(_currentTime.ToString() + " Timer");
-        tmpText.text = textTemplate;
+        tmpText.text = textPattern;
     }
     private void OnDisable()
     {
