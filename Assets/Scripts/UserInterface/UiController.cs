0bccb43ec0c40139eda221c73e34f01d4eb93ac9
Ilya Molchanov <lolkers.ilya@gmail.com>
2024-06-01 18:11:29 +0300

    Make ui controller

--- Assets/Scripts/UserInterface/UiController.cs
+++ Assets/Scripts/UserInterface/UiController.cs
@@ -6,17 +6,34 @@ public class UiController : MonoBehaviour
     [HideInInspector] public bool Easy, Normal, Hard, GamemodeChosen;
     [HideInInspector] public int LvlMode;
 
-
+    [SerializeField] private GameObject easyModeLvl;
+    [SerializeField] private GameObject normalModeLvl;
+    [SerializeField] private GameObject hardModeLvl;
+    [Space]
+    [SerializeField] private GameObject inGameMenuCanvas;
+    [SerializeField] private GameObject mainMenuCanvas;
+    [SerializeField] private GameObject choosingGameModeCanvas;
+    [Space]
     [SerializeField] private TimerCounter timer;
     public void GamemodeEasy()
     {
         LvlMode = 1;
+
+        choosingGameModeCanvas.SetActive(false);
+        easyModeLvl.SetActive(true);
+        inGameMenuCanvas.SetActive(true);
+        
         StartCoroutine(timer.StartGame(LvlMode));
         Debug.Log("Easy");
     }
     public void GamemodeNormal()
     {
         LvlMode = 2;
+
+        choosingGameModeCanvas.SetActive(false);
+        normalModeLvl.SetActive(true);
+        inGameMenuCanvas.SetActive(true);
+
         StartCoroutine(timer.StartGame(LvlMode));
         Debug.Log("Medium");
     }
@@ -24,11 +41,32 @@ public class UiController : MonoBehaviour
     public void GamemodeHard()
     {
         LvlMode = 3;
+
+        choosingGameModeCanvas.SetActive(false);
+        hardModeLvl.SetActive(true);
+        inGameMenuCanvas.SetActive(true);
+        
         StartCoroutine(timer.StartGame(LvlMode));
-        Debug.Log("Hard"); 
+        Debug.Log("Hard");
     }
-    public void Pause() => Time.timeScale = 0;
-    public void Continue() => Time.timeScale = 1;
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+    public void Continue()
+    {
+        Time.timeScale = 1;
+    }
+    public void LeaveToMainMenu()
+    {
+        easyModeLvl.SetActive(false);
+        normalModeLvl.SetActive(false);
+        hardModeLvl.SetActive(false);
+        inGameMenuCanvas.SetActive(false);
+        choosingGameModeCanvas.SetActive(false);
+
+        mainMenuCanvas.SetActive(true);
+    }
     public void TryAgain()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
